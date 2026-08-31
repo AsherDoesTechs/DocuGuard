b3029c1f6b4d584920d7eaf8728ffc4dcf26d82b
Asherbasco93@gmail.com <Asherbasco93@gmail.com>
2026-08-31 12:55:11 +0800

    Changes to the FCM - adjusts some ENV FIle.

--- DocuGuard-Server/app.js
+++ DocuGuard-Server/app.js
@@ -39,4 +39,8 @@ app.use("/api/export", exportRoutes);
 // 4. Centralized Error Handler (Must be registered LAST after all routes)
 app.use(errorHandler);
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- DocuGuard-Server/config/firebase.js
+++ DocuGuard-Server/config/firebase.js
@@ -1,8 +1,13 @@
-const { initializeApp, cert } = require("firebase-admin/app");
-const serviceAccount = require("./serviceAccountKey.json");
+const { initializeApp, cert, getApps } = require("firebase-admin/app");
 
-const app = initializeApp({
-  credential: cert(serviceAccount),
-});
+const app = getApps().length
+  ? getApps()[0]
+  : initializeApp({
+      credential: cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      }),
+    });
 
 module.exports = app;
